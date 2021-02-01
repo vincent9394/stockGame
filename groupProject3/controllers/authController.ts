b0418bb053c67110f12d95797e6a9e8751233985
Bruce CHCHEUNG <chcheung.email@gmail.com>
2021-02-01 10:33:02 +0800

    removed server files

--- groupProject3/controllers/authController.ts
+++
@@ -1,9 +0,0 @@
-import type express from 'express'
-
-export class AuthController{
-    public constructor(private authService: AuthService){}
-
-    public login = async (req: express.Request, res: express.Response) => {
-         
-    }
-}
