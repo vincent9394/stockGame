bb1e70418d60610fcddcda468d598072fef510f1
Bruce CHCHEUNG <chcheung.email@gmail.com>
2021-02-02 13:42:38 +0800

    updated .gitignore for knex migrations

---
+++ groupProject3/groupProject3Backend/migrations/20210201151335_postgresql-initial-schema.ts
@@ -0,0 +1,61 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('user', (table) =>{
+        table.increments();
+        table.string('name');
+        table.string('password');
+        table.string('email');
+        table.decimal('cash_in_hand', 10, 4);
+    })
+
+    await knex.schema.createTable('trade_cart', (table) => {
+        table.increments();
+        table.integer('user_id');
+        table.string('stock_symbol');
+        table.boolean('isBuy');
+        table.decimal('price', 6, 2);
+        table.decimal('shares', 10, 2);
+    })
+
+    await knex.schema.createTable('trade', (table) => {
+        table.increments();
+        table.integer('user_id');
+        table.string('stock_symbol');
+        table.boolean('isBuy');
+        table.decimal('price', 6, 2);
+        table.decimal('shares', 10, 2);
+        table.integer('portfolio_id');
+    })
+
+    await knex.schema.createTable('stock', (table) => {
+        table.increments();
+        table.dateTime('date');
+        table.string('stock_symbol');
+        table.decimal('open', 6, 2);
+        table.decimal('high', 6, 2);
+        table.decimal('low', 6, 2);
+        table.decimal('close', 6, 2);
+        table.decimal('volume_ltc', 10, 2);
+        table.decimal('volume_usd', 10, 2);
+    })
+
+    await knex.schema.createTable('portfolio', (table) => {
+        table.increments();
+        table.integer('user_id');
+        table.string('stock_symbol');
+        table.decimal('shares', 10, 2);
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('portfolio');
+    await knex.schema.dropTable('stock');
+    await knex.schema.dropTable('trade');
+    await knex.schema.dropTable('trade_cart');
+    await knex.schema.dropTable('user');
+
+}
+
