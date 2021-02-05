7965d8e0796bf0b6e0685c722bd9c11056f995df
Bruce CHCHEUNG <chcheung.email@gmail.com>
2021-02-05 20:02:13 +0800

    added fetching alphavantage api

---
+++ groupProject3/groupProject3Backend/alphavantage.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let tickets: string[] = [
+    "AAPL",
+    "GOOGL",
+    "TSLA",
+];
+
+async function downloadData() {
+    let datas = [];
+    for (let ticket of tickets) {
+        // you need to apply for a token from alphavantage to obtain a key for the following api:
+        const request = await fetch(
+            `https://www.alphavantage.co/query?function=${timePeriod}&symbol=${ticket}&interval=1min&apikey=${process.env.ALPHA_VANTAGE_API_KEY}`
+        );
+        // const request = await fetch(
+        //     `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo`
+        // );
+        const data = await request.json();
+        console.log(`Printing data ${ticket}`)
+        console.log(new Date())
+        console.log(data)
+        datas.push(data)
+    }
+    return datas;
+}
+
+setInterval(() => {
+    let data = downloadData();
+    console.log(data);
+}, 60 * 1000 + Math.random() * 10 * 1000);
+
