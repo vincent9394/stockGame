b390a843cef35f5cf88b88e60ba33b5f8e533b87
Ricky <c212ds@gmail.com>
2021-02-08 14:22:29 +0800

    bug fix

--- groupProject3/groupProject3Backend/stocks/stock-service.ts
+++ groupProject3/groupProject3Backend/stocks/stock-service.ts
@@ -1,17 +1,18 @@
 import * as Knex from 'knex';
 export class StockService{
     constructor(private knex:Knex){}
-async stockTrading(user_id:number,stock_symbol:string,is_buy:Boolean,price:number,shares:number){    //problem trading action will be delay   it will be done only the tradingCart accept time problem
+async stockTrading(user_id:number,stock_symbol:string,transaction_type_id:number,price:number,shares:number){    //problem trading action will be delay   it will be done only the tradingCart accept time problem
     return await this.knex.transaction(async (trx)=>{
         const newTransactionID=await trx.insert({    
             user_id:user_id,
             stock_symbol:stock_symbol,
-            is_buy:is_buy,
+            transaction_type_id,
             price:price,
             shares:shares,   
         }).into('trade').returning('id')
          const OriginalAccountBalance=await trx.select('cash_in_hand').from('users').where('id',user_id)[0];  
-         const NewAccountBalance=is_buy?OriginalAccountBalance-(price*shares):OriginalAccountBalance+(price*shares);
+         const NewAccountBalance=OriginalAccountBalance+(price*shares)
+         //is_buy?OriginalAccountBalance-(price*shares):OriginalAccountBalance+(price*shares);
             await trx('users').update({
                 cash_in_hand:NewAccountBalance,
             }).where('id',user_id)
@@ -20,11 +21,12 @@ if(checkPortfolio.length===0){
        await trx.insert({  
         user_id:user_id,
         stock_symbol:stock_symbol,
-        shares:is_buy?shares:-shares,
+        shares:shares,
     }).into('portfolio')
 }else{
     const OriginalShares=await trx.select('shares').from('users').where('id',user_id).where('stock_symbol',stock_symbol)[0]; //maybe incorrect by trx                        //update the balance
-         const NewShares=is_buy?OriginalShares+shares:OriginalShares-shares;
+         const NewShares=OriginalShares+shares
+         //is_buy?OriginalShares+shares:OriginalShares-shares;
             await trx('users').update({
             shares:NewShares,
             }).where('id',user_id)
@@ -32,27 +34,34 @@ if(checkPortfolio.length===0){
          return [newTransactionID[0],NewAccountBalance]
     })
 }
-async AddStockTradingInstruction(user_id:number,stock_symbol:string,is_buy:Boolean,price:number,shares:number,dateTime:Date){ 
+async AddStockTradingInstruction(user_id:number,stock_symbol:string,transaction_type_id:number,price:number,shares:number,dateTime:Date){ 
     return await this.knex('trade_cart').insert({
         user_id:user_id,
         stock_symbol:stock_symbol,
-        is_buy:is_buy,
+        transaction_type_id:transaction_type_id,
         price:price,
         shares:shares,
-        expiration_date:dateTime,
+        transaction_status_id:2,
+        exp_datetime:dateTime,
     }).returning('id')
 }
 async loadSearchingResult(stockSymbol:string|null,stockName:string|null){   //get the result time by time to draw a graph
     if (stockSymbol!==null){
         return await this.knex.select('*').from('stock_history').where('stock_symbol',stockSymbol);
     }else if(stockName!==null){
-        return await this.knex.select('*').from('stock_history').where('stockName',stockName);
+        const ResultStockSymbol=await this.knex('stock_symbol').from('stock_info').where('name',stockName)
+        console.log(ResultStockSymbol)
+        if(ResultStockSymbol.length>0){
+        return await this.knex.select('*').from('stock_history').where('stock_symbol',ResultStockSymbol[0].stock_symbol);
+        }else{
+            return[]
+        }
     }else{
         return []
     }
 }
 async loadAllStockInfo(){   //show all basic info with latest  ,problem
-    return await this.knex.select('*').from('stock')
+    return await this.knex.select('*').from('stock_current')
 }
 
 async actionToWatchList(user_id:number,stock_symbol:string,watchListAction:string){  //maybe use for watch list
--- groupProject3/groupProject3Backend/testing/stock-service.test.ts
+++ groupProject3/groupProject3Backend/testing/stock-service.test.ts
@@ -11,13 +11,13 @@ describe('stockService integrated with database',()=>{
         await knex.seed.run();
     });
     it('can work with Buy transaction',async()=>{
-        const addTrading=await stockService.stockTrading(1,'abc',true,20,400)
-        expect(addTrading[0]).toHaveLength(1)
+        const addTrading=await stockService.stockTrading(1,'abc',1,20,400)
+        expect(addTrading).toHaveLength(2)
 
     })
     it('can work with sell transaction',async()=>{
-        const addTrading=await stockService.stockTrading(1,'abc',false,20,400)
-        expect(addTrading[0]).toHaveLength(1)
+        const addTrading=await stockService.stockTrading(1,'abc',2,20,400)
+        expect(addTrading).toHaveLength(2)
 
     })
     it('can load SearchResult by stockSymbol',async()=>{
@@ -27,9 +27,9 @@ describe('stockService integrated with database',()=>{
 
     })
     it('can load SearchResult by stockName',async()=>{
-        const loadResult=await stockService.loadSearchingResult(null,"APPLE")
-        expect(loadResult).toHaveLength(1)
-        expect(loadResult[0].stockName).toBe("APPLE")
+        const loadResult=await stockService.loadSearchingResult(null,"Apple Inc")
+        expect(loadResult).toHaveLength(5)
+        expect(loadResult[0].stockName).toBe("Apple Inc")
 
     })
     it("can't load SearchResult by IncorrectStockName",async()=>{
@@ -44,13 +44,13 @@ describe('stockService integrated with database',()=>{
     })
     it("can't load SearchResult if there are no name and no symbol",async()=>{
         const loadResult=await stockService.loadSearchingResult(null,null)
-        expect(loadResult).toBe(null)
+        expect(loadResult).toHaveLength(0)
 
     })
 
     it('can load all the stocks info In home',async()=>{ //need adjust expect
         const loadStockInfo=await stockService.loadAllStockInfo()
-        expect(loadStockInfo).toHaveLength(5)
+        expect(loadStockInfo).toHaveLength(3)
 
     })
     afterAll( ()=>{
